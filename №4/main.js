cc4cb0da18518704d2ed1221c68052cdfb526da1
Nikita_Korotkevich <Nikkor2001@gmail.com>
2021-11-15 23:21:24 +0300

    hometask #4

---
+++ №4/main.js
@@ -0,0 +1,21 @@
+let VowelsList = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"];
+
+function getVowels(VowelsList) {
+    let VowelsSum = 0;
+
+    return function (x) {
+
+      for (let i of VowelsList.toLowerCase()) {
+        if (x.includes(i)) {
+            VowelsSum += 1;
+            console.log(i);
+        }
+      }
+  
+      return VowelsSum;
+    };
+  }
+
+  let SomeString = prompt(`Введите строку:`);
+  let new_Search = getVowels(SomeString);
+  alert(`количество гласных букв:${new_Search(VowelsList)}`);
